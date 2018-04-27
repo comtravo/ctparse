25b6d048aed99c3724eccf09e033512976e69226
Sebastian Mika <sebastianmika@users.noreply.github.com>
2018-04-27 17:32:53 +0200

    bump version to 0.0.4 (#6)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.0.3'
+version = '0.0.4'
 
 setup(
     name='ctparse',
