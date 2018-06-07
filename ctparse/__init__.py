f64e4641208afff2efa0b06fc17e02e2860b271b
Sebastian Mika <sebastian.mika@comtravo.com>
2018-06-07 17:06:05 +0200

    fix __init__.py for bumpversion

--- ctparse/__init__.py
+++ ctparse/__init__.py
@@ -3,4 +3,8 @@
 .. moduleauthor:: Comtravo
 
 """
+__author__ = """Sebastian Mika/Comtravo"""
+__email__ = 'sebastian.mika@comtravo.com'
+__version__ = '0.0.8'
+
 from . ctparse import ctparse  # noqa
