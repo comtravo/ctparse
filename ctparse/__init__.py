4ac78878927613587e03e8b1862adf6a00e0fd57
Sebastian Mika <sebastian.mika@comtravo.com>
2018-06-07 17:11:25 +0200

    Revert "fix __init__.py for bumpversion"
    This reverts commit f64e4641208afff2efa0b06fc17e02e2860b271b.

--- ctparse/__init__.py
+++ ctparse/__init__.py
@@ -3,8 +3,4 @@
 .. moduleauthor:: Comtravo
 
 """
-__author__ = """Sebastian Mika/Comtravo"""
-__email__ = 'sebastian.mika@comtravo.com'
-__version__ = '__version__ = '0.0.8''
-
 from . ctparse import ctparse  # noqa
