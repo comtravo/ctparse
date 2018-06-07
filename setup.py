081e4a21cb2734cd8c85da3601aa3a34bd37fef6
Sebastian Mika <sebastian.mika@comtravo.com>
2018-06-07 18:48:28 +0200

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/comtravo/ctparse',
-    version='0.0.11',
+    version='0.0.12',
     zip_safe=False,
 )
