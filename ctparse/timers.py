d6979d8cb132ef4c0f8740e06c88c21170d9dc60
Gabriele Lanaro <gabriele.lanaro@comtravo.com>
2019-10-22 10:36:48 +0200

    Extract timers as a separate module

---
+++ ctparse/timers.py
@@ -0,0 +1,66 @@
+"""Utilities for tracking time spent in functions.
+
+Although this module is not part of the public API, it is used in various parts of
+the ctparse package.
+
+"""
+from time import perf_counter
+from typing import Callable, TypeVar, Union, Tuple
+from functools import wraps
+
+T = TypeVar('T')
+
+
+def timeout(timeout: Union[float, int]) -> Callable[[], None]:
+    """Generate a functions that raises an exceptions if a timeout has passed.
+
+    Example:
+
+        sentinel = timeout(1.0)
+        time.sleep(0.5)
+        sentinel() # Do nothing
+        time.sleep(0.6)
+        sentinel() # Raises CTParseTimeoutException
+
+    :param timeout:
+       time in seconds. If it is equal to zero, it means to never raise an exception.
+    :returns:
+        A function that raises a `CTParseTimeoutException` if `timeout` seconds have expired.
+    """
+    start_time = perf_counter()
+
+    def _tt():
+        if timeout == 0:
+            return
+        if perf_counter() - start_time > timeout:
+            raise CTParseTimeoutError()
+    return _tt
+
+
+def timeit(f: Callable[..., T]) -> Callable[..., Tuple[T, float]]:
+    """Wrapper to time a function.
+
+    The wrapped function is modified so that it returns a tuple `(f(args), t)`
+    where `t` the time in seconds the function call took to run.
+
+    Example:
+
+        def fun(x):
+            return x * x
+
+        result, exec_time = timeit(fun)(3)
+
+    """
+    @wraps(f)
+    def _wrapper(*args, **kwargs):
+        start_time = perf_counter()
+        res = f(*args, **kwargs)
+        return res, perf_counter() - start_time
+    return _wrapper
+
+
+# NOTE: TimeoutError is a built-in exception that means that
+# system function timed out at the system level. Hence we opt
+# for a custom exception.
+class CTParseTimeoutError(Exception):
+    """Exception raised by the `timeout` function."""
---
+++ tests/test_timers.py
@@ -0,0 +1,22 @@
+from ctparse.timers import timeout, CTParseTimeoutError, timeit
+from unittest import TestCase
+import time
+
+
+class TimersTest(TestCase):
+
+    def test_timeout(self):
+        t_fun = timeout(0.5)
+        with self.assertRaises(CTParseTimeoutError):
+            time.sleep(1.0)
+            t_fun()
+        t_fun = timeout(0)
+        t_fun()  # all good
+
+    def test_timeit(self):
+        def fun(x):
+            return x * x
+
+        result, elapsed = timeit(fun)(3)
+        self.assertEqual(result, 9)
+        self.assertIsInstance(elapsed, float)
