183e175b2ba93e059b1bf2262856e4b4a9f69b90
Gabriele Lanaro <gabriele.lanaro@comtravo.com>
2019-10-22 10:40:49 +0200

    Make comment refer to the right line

--- ctparse/ctparse.py
+++ ctparse/ctparse.py
@@ -10,8 +10,8 @@ from tqdm import tqdm
 
 from .nb import NB
 from .rule import _regex, rules
-# Avoid collision with variable "timeout"
 from .timers import CTParseTimeoutError, timeit
+# Avoid collision with variable "timeout"
 from .timers import timeout as timeout_
 from .types import RegexMatch
 
