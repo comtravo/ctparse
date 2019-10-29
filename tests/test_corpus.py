6505990cde225e04cdc3693d40fcd631701c5f5e
Gabriele Lanaro <gabriele.lanaro@comtravo.com>
2019-10-29 14:05:50 +0100

    Make mypy happy

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,11 +1,16 @@
 import pytest
 
-from ctparse.model import run_corpus
+from ctparse.corpus import run_corpus
 from ctparse.time.corpus import corpus
 
 
 def test_run_corpus() -> None:
-    run_corpus(corpus)
+    """The corpus passes if ctparse generates the desired
+    solution for each test at least once. Otherwise it fails.
+    """
+    X, y = run_corpus(corpus)
+    assert isinstance(y[0], bool)
+    assert isinstance(X[0][0], str)
 
 
 def test_run_corpus_failure() -> None:
