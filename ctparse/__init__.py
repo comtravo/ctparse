f8f00e39a749adaf9020f3b22c07e549515866e5
Gabriele Lanaro <gabriele.lanaro@comtravo.com>
2019-10-30 10:18:16 +0100

    Bump version: 0.0.41 → 0.0.42

--- ctparse/__init__.py
+++ ctparse/__init__.py
@@ -5,6 +5,6 @@
 """
 __author__ = """Sebastian Mika"""
 __email__ = 'sebastian.mika@comtravo.com'
-__version__ = '0.0.41'
+__version__ = '0.0.42'
 
 from . ctparse import ctparse, ctparse_gen  # noqa
