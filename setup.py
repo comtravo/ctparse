caef34d83d76a4203b7da3855475436ba53f421f
Gabriele Lanaro <gabriele.lanaro@comtravo.com>
2020-02-26 14:24:53 +0100

    Bump version: 0.0.45 → 0.0.46

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/comtravo/ctparse',
-    version='0.0.45',
+    version='0.0.46',
     zip_safe=False,
 )
